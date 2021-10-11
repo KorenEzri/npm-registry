85b7700583504ea20d4f6130ea69d48776a01ecd
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-10-11 12:59:05 +0300

    ci: 🎡 Change from BrowserRouter to HashRouter for Ci/CD on GH

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,14 +1,5 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
-import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 
 import { GlobalStyle } from 'styles/global-styles';
 
@@ -19,7 +10,7 @@ import { useTranslation } from 'react-i18next';
 export function App() {
   const { i18n } = useTranslation();
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Helmet
         titleTemplate="%s - NPM-registry"
         defaultTitle="NPM-registry"
@@ -33,6 +24,6 @@ export function App() {
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
